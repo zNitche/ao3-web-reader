01c7e0d6c47b0cdc687b8215f963a7f450307404
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-27 21:51:33 +0100

    api routes naming tweaks + fixed login required decorators

--- ao3_web_reader/blueprints/api/routes.py
+++ ao3_web_reader/blueprints/api/routes.py
@@ -12,8 +12,8 @@ def healthcheck():
     return Response(status=200)
 
 
+@api.route("/sync-status", methods=["GET"])
 @login_required
-@api.route("/sync_status", methods=["GET"])
 def sync_status():
     sync_process_status = current_app.processes_manager.get_background_process_data("WorksUpdaterProcess")
     sync_process_running = True if sync_process_status and sync_process_status.get(ProcessesConsts.IS_RUNNING) else False
@@ -24,8 +24,8 @@ def sync_status():
     }
 
 
+@api.route("/running-scraping-processes", methods=["GET"])
 @login_required
-@api.route("/running_scraping_processes", methods=["GET"])
 def running_scraping_processes():
     user_id = flask_login.current_user.id
     running_processes = current_app.processes_manager.get_processes_data_for_user("ScraperProcess", user_id)
