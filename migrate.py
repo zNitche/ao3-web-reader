08cecac430e530a21ef3d72494a596c768b3a650
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-29 20:50:16 +0100

    migrations script refactoring

--- migrate.py
+++ migrate.py
@@ -1,22 +1,13 @@
-import flask_migrate
-import os
-from ao3_web_reader import create_app
-from ao3_web_reader import db, migrate
+from ao3_web_reader.db import Database, migrations
+from config import Config
 
 
 def main():
-    app = create_app(detached=True)
+    db = Database()
+    db.setup(Config.DATABASE_URI)
 
-    migrations_dir_path = app.config["MIGRATIONS_DIR_PATH"]
-
-    with app.app_context():
-        migrate.init_app(app, db, directory=migrations_dir_path)
-
-        if not os.path.exists(migrations_dir_path):
-            flask_migrate.init(migrations_dir_path)
-
-        flask_migrate.migrate(migrations_dir_path)
-        flask_migrate.upgrade(migrations_dir_path)
+    migrations.init_migrations(Config.MIGRATIONS_DIR_PATH, db.engine)
+    migrations.migrate(Config.MIGRATIONS_DIR_PATH, db.engine)
 
 
 if __name__ == '__main__':
