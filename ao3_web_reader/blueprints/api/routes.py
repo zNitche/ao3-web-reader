7b854114b8b1edd4d65b0d4a29ceb28922b5b969
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-30 22:33:15 +0100

    api route decorators minor tweaks

--- ao3_web_reader/blueprints/api/routes.py
+++ ao3_web_reader/blueprints/api/routes.py
@@ -2,7 +2,6 @@ import flask_login
 from flask import Blueprint, Response
 from ao3_web_reader import processes_manager
 from ao3_web_reader.consts import ProcessesConsts
-from flask_login import login_required
 
 
 api = Blueprint("api", __name__, template_folder="templates", static_folder="static", url_prefix="/api")
@@ -14,7 +13,7 @@ def healthcheck():
 
 
 @api.route("/sync-status", methods=["GET"])
-@login_required
+@flask_login.login_required
 def sync_status():
     sync_process_status = processes_manager.get_background_process_data("WorksUpdaterProcess")
     sync_process_running = True if sync_process_status and sync_process_status.get(ProcessesConsts.IS_RUNNING) else False
@@ -26,7 +25,7 @@ def sync_status():
 
 
 @api.route("/running-scraping-processes", methods=["GET"])
-@login_required
+@flask_login.login_required
 def running_scraping_processes():
     user_id = flask_login.current_user.id
     running_processes = processes_manager.get_processes_data_for_user("ScraperProcess", user_id)
