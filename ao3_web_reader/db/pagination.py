4a34a80f441eb1c098455a4f123a8f3ed6c4dc93
zNitche <53448802+zNitche@users.noreply.github.com>
2024-11-07 19:23:08 +0100

    tweaked pagination internal logic

--- ao3_web_reader/db/pagination.py
+++ ao3_web_reader/db/pagination.py
@@ -1,17 +1,16 @@
 class Pagination:
     def __init__(self, query, page_id, items_per_page=25):
         self.__query = query
+
         self.page_id = page_id
         self.items_per_page = items_per_page
-
-        self.items = []
 
         self.has_next = False
         self.has_prev = page_id > 1
         self.prev_num = page_id - 1 if self.has_prev else 1
         self.next_num = page_id
 
-        self.__query_items()
+        self.items = self.__query_items()
 
     def __query_items(self):
         total_items = self.__query.count()
@@ -20,4 +19,4 @@ class Pagination:
         self.has_next = total_items > offset + self.items_per_page
         self.next_num = self.page_id + 1 if self.has_next else self.page_id
 
-        self.items = self.__query.limit(self.items_per_page).offset(offset).all()
+        return self.__query.limit(self.items_per_page).offset(offset).all()
