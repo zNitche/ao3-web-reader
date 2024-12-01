4095de642c0ce41bcb3bfa044287f6dfafb93231
zNitche <53448802+zNitche@users.noreply.github.com>
2024-12-01 21:04:33 +0100

    calibre compatible html exporter (refactoring of current one)

--- ao3_web_reader/ebook_exporter/html/html_exporter.py
+++ ao3_web_reader/ebook_exporter/html/html_exporter.py
@@ -1,6 +1,5 @@
 import os
 import config
-import sys
 import copy
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -37,6 +36,35 @@ class HTMLExporter:
     def __convert_date(self, date: datetime, format="%d-%m-%Y"):
         return date.strftime(format)
 
+    def __prettify(self, content: str):
+        soup = BeautifulSoup(content)
+        return soup.prettify()
+
+    def __build_table_of_contents(self, chapters: list[models.Chapter]):
+        core_template = self.__load_template("core")
+        item_template = self.__load_template("chapter_item")
+
+        core_template = self.__replace_template_value(core_template, "work_name", self.work.name)
+        core_template = self.__replace_template_value(core_template, "work_added_date",
+                                                      self.__convert_date(self.work.date))
+        core_template = self.__replace_template_value(core_template, "work_last_update",
+                                                      self.__convert_date(self.work.last_updated))
+        core_template = self.__replace_template_value(core_template, "work_description", self.work.description)
+
+        results = []
+
+        for ind, chapter in enumerate(chapters, start=1):
+            template = copy.copy(item_template)
+
+            template = self.__replace_template_value(template, "href", f"{ind}.html")
+            template = self.__replace_template_value(template, "title", chapter.title)
+
+            results.append(template)
+
+        core_template = self.__replace_template_value(core_template, "items", "".join(results))
+
+        return core_template
+
     def __build_chapter(self, chapter: models.Chapter, template: str):
         self.logger.info(f"building {chapter.title}...")
 
@@ -48,39 +76,38 @@ class HTMLExporter:
 
         return template
 
-    def export(self, output_file: sys.stdout, prettify=True):
+    def __write_file(self, path: str, content: str, prettify: bool):
+        with open(path, "w") as file:
+            if prettify:
+                content = self.__prettify(content)
+
+            file.write(content)
+
+    def export(self, output_dir_path: str, prettify=True):
         self.logger.info("stating export process...")
 
         try:
-            core_template = self.__load_template("core")
+            self.logger.info("building table of contents...")
+            table_of_contents = self.__build_table_of_contents(self.work.chapters)
 
-            core_template = self.__replace_template_value(core_template, "work_name", self.work.name)
-            core_template = self.__replace_template_value(core_template, "work_added_date",
-                                                          self.__convert_date(self.work.date))
-            core_template = self.__replace_template_value(core_template, "work_last_update",
-                                                          self.__convert_date(self.work.last_updated))
-            core_template = self.__replace_template_value(core_template, "work_description", self.work.description)
-
-            self.logger.info("building chapters")
+            self.logger.info("building chapters...")
             chapter_template = self.__load_template("chapter")
-
             chapters = []
 
             for chapter in self.work.chapters:
-                chapter_template_cp = copy.copy(chapter_template)
+                self.logger.info(f"building html for {chapter.title}...")
 
+                chapter_template_cp = copy.copy(chapter_template)
                 chapters.append(self.__build_chapter(chapter, chapter_template_cp))
 
-            core_template = self.__replace_template_value(core_template, "chapters", " ".join(chapters))
-            self.logger.info(f"saving {self.work.name} exported content...")
+            self.logger.info("saving exported data...")
 
-            if prettify:
-                soup = BeautifulSoup(core_template)
-                core_template = soup.prettify()
+            self.__write_file(os.path.join(output_dir_path, "index.html"), table_of_contents, prettify)
 
-            output_file.write(core_template)
+            for ind, chapter in enumerate(chapters, start=1):
+                self.__write_file(os.path.join(output_dir_path, f"{ind}.html"), chapter, prettify)
 
-            self.logger.info(f"exported content has been saved...")
+            self.logger.info(f"exported data has been saved...")
 
         except Exception as e:
             self.logger.exception(f"error while exporting {self.work.name}")
