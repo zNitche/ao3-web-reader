39ed72841f9f419ed2f44c8fc0372fc284267da1
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-21 21:05:31 +0100

    refactor: html_exporter code formatting tweaks

--- ao3_web_reader/ebook_exporter/html/html_exporter.py
+++ ao3_web_reader/ebook_exporter/html/html_exporter.py
@@ -13,14 +13,17 @@ class HTMLExporter:
         self.work = work
 
         self.extension = "html"
+        self.logs_path = os.path.join(
+            config.Config.LOGS_DIR_PATH, "ebook_exporters")
 
-        self.logger = Logger(logger_name=f"HTMLExporter_{self.user_id}",
+        self.logger = Logger(logger_name=f"HTMLExporter_{user_id}",
                              logs_filename="HTMLExporter.log",
-                             logs_path=os.path.join(config.Config.LOGS_DIR_PATH, "ebook_exporters"),
+                             logs_path=self.logs_path,
                              backup_log_files_count=1)
 
     def __load_template(self, type: str):
-        template_path = os.path.join(config.APP_ROOT, "ebook_exporter", "html", "templates", f"{type}.html")
+        template_path = os.path.join(
+            config.APP_ROOT, "ebook_exporter", "html", "templates", f"{type}.html")
 
         if not os.path.exists(template_path):
             raise Exception(f"template file doesn't exist: {template_path}")
@@ -44,33 +47,41 @@ class HTMLExporter:
         core_template = self.__load_template("core")
         item_template = self.__load_template("chapter_item")
 
-        core_template = self.__replace_template_value(core_template, "work_name", self.work.name)
+        core_template = self.__replace_template_value(
+            core_template, "work_name", self.work.name)
         core_template = self.__replace_template_value(core_template, "work_added_date",
                                                       self.__convert_date(self.work.date))
         core_template = self.__replace_template_value(core_template, "work_last_update",
                                                       self.__convert_date(self.work.last_updated))
-        core_template = self.__replace_template_value(core_template, "work_description", self.work.description)
+        core_template = self.__replace_template_value(
+            core_template, "work_description", self.work.description)
 
         results = []
 
         for ind, chapter in enumerate(chapters, start=1):
             template = copy.copy(item_template)
 
-            template = self.__replace_template_value(template, "href", f"{ind}.html")
-            template = self.__replace_template_value(template, "title", chapter.title)
+            template = self.__replace_template_value(
+                template, "href", f"{ind}.html")
+            template = self.__replace_template_value(
+                template, "title", chapter.title)
 
             results.append(template)
 
-        core_template = self.__replace_template_value(core_template, "items", "".join(results))
+        core_template = self.__replace_template_value(
+            core_template, "items", "".join(results))
 
         return core_template
 
     def __build_chapter(self, chapter: models.Chapter, template: str):
         self.logger.info(f"building {chapter.title}...")
 
-        template = self.__replace_template_value(template, "title", chapter.title)
-        template = self.__replace_template_value(template, "date", self.__convert_date(chapter.date))
-        template = self.__replace_template_value(template, "content", chapter.text)
+        template = self.__replace_template_value(
+            template, "title", chapter.title)
+        template = self.__replace_template_value(
+            template, "date", self.__convert_date(chapter.date))
+        template = self.__replace_template_value(
+            template, "content", chapter.text)
 
         self.logger.info(f"{chapter.title} has been completed")
 
@@ -88,7 +99,8 @@ class HTMLExporter:
 
         try:
             self.logger.info("building table of contents...")
-            table_of_contents = self.__build_table_of_contents(self.work.chapters)
+            table_of_contents = self.__build_table_of_contents(
+                self.work.chapters)
 
             self.logger.info("building chapters...")
             chapter_template = self.__load_template("chapter")
@@ -98,14 +110,17 @@ class HTMLExporter:
                 self.logger.info(f"building html for {chapter.title}...")
 
                 chapter_template_cp = copy.copy(chapter_template)
-                chapters.append(self.__build_chapter(chapter, chapter_template_cp))
+                chapters.append(self.__build_chapter(
+                    chapter, chapter_template_cp))
 
             self.logger.info("saving exported data...")
 
-            self.__write_file(os.path.join(output_dir_path, "index.html"), table_of_contents, prettify)
+            self.__write_file(os.path.join(
+                output_dir_path, "index.html"), table_of_contents, prettify)
 
             for ind, chapter in enumerate(chapters, start=1):
-                self.__write_file(os.path.join(output_dir_path, f"{ind}.html"), chapter, prettify)
+                self.__write_file(os.path.join(
+                    output_dir_path, f"{ind}.html"), chapter, prettify)
 
             self.logger.info(f"exported data has been saved...")
 
