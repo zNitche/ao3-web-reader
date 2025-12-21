d6732b4b213f01dbb8dde176b1f24b3f98dd6154
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-21 20:31:35 +0100

    refactor: ZipTagOnTheFly - creating nested tmp dir

--- ao3_web_reader/modules/extra/zip_tag_on_the_fly.py
+++ ao3_web_reader/modules/extra/zip_tag_on_the_fly.py
@@ -7,15 +7,15 @@ from ao3_web_reader.modules.extra.zip_on_the_fly import ZipFileStream, ZipOnTheF
 
 
 class ZipTagOnTheFly(ZipOnTheFly):
-    def __init__(self, user_id, works, chunk_size=1_000_000):
+    def __init__(self, user_id, tag_name, works, chunk_size=1_000_000):
         super().__init__(chunk_size=chunk_size)
 
+        self.tag_name = tag_name
         self.works = works
         self.user_id = user_id
 
-    def __zip_work(self, tmp_dir_name, work):
+    def __zip_work(self, files_path, work):
         exporter = HTMLExporter(self.user_id, work)
-        files_path = os.path.join(tempfile.gettempdir(), tmp_dir_name)
 
         exporter.export(files_path, prettify=True)
         work_name = works_utils.serialize_work_name(work.name)
@@ -31,15 +31,23 @@ class ZipTagOnTheFly(ZipOnTheFly):
         return archive_path, archive_name
 
     def generator(self):
-        with ZipFileStream() as zip_stream:
-            with ZipFile(zip_stream, mode="w") as zip_file:
-                for work in self.works:
-                    with tempfile.TemporaryDirectory() as tmp_dir_name:
+        core_dir_prefix = f"{str(self.user_id)}_{str(self.tag_name)}"
+
+        with tempfile.TemporaryDirectory(prefix=core_dir_prefix) as tmp_dir_name:
+            with ZipFileStream() as zip_stream:
+                with ZipFile(zip_stream, mode="w") as zip_file:
+                    for work in self.works:
+                        dir_path = os.path.join(
+                            tempfile.gettempdir(), tmp_dir_name, str(work.id))
+
+                        if not os.path.exists(dir_path):
+                            os.mkdir(dir_path)
+
                         work_archive_path, archive_name = self.__zip_work(
-                            tmp_dir_name, work)
+                            dir_path, work)
 
                         yield from self._handle_file(zip_file=zip_file, zip_stream=zip_stream,
-                                                      file_name=archive_name, file_path=work_archive_path)
+                                                     file_name=archive_name, file_path=work_archive_path)
 
             # drain stream
             yield zip_stream.get()
