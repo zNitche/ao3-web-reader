cf671bc08e88692e7d516b4003b2aa19074ab8d4
zNitche <53448802+zNitche@users.noreply.github.com>
2025-12-21 21:05:09 +0100

    refactor: send_tag_as_zip() tweaks

--- ao3_web_reader/utils/requests_utils.py
+++ ao3_web_reader/utils/requests_utils.py
@@ -1,4 +1,4 @@
-from flask import Response
+from flask import Response, stream_with_context
 from ao3_web_reader.modules.extra import ZipTagOnTheFly
 
 
@@ -6,6 +6,10 @@ def send_tag_as_zip(user_id, works, tag_name):
     zip_on_the_fly = ZipTagOnTheFly(
         user_id=user_id, works=works, tag_name=tag_name)
 
-    return Response(zip_on_the_fly.generator(), mimetype="application/zip", headers={
-        "Content-Disposition": f"attachment; filename={tag_name}.zip"
-    })
+    headers = {
+        "Content-Disposition": f"attachment; filename={tag_name}.zip",
+    }
+
+    generator = zip_on_the_fly.generator()
+
+    return Response(generator, mimetype="application/zip", headers=headers)
